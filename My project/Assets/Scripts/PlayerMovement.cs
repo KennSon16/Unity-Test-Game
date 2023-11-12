97b263a07d1933dd4b8b3ff7a673de67b5a482ce
Kenn Son <kennson1616@gmail.com>
2023-11-12 15:48:43 -0800

    Fixed Rotation and Jump Bug

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
     private SpriteRenderer sprite;
     private Animator anim;
+
+    [SerializeField] private LayerMask jumpableGround;
+
     private float dirX;
     [SerializeField]private float moveSpeed = 7f;
     [SerializeField]private float jumpForce = 17f;
@@ -22,6 +26,7 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
     }
@@ -32,7 +37,7 @@ public class PlayerMovement : MonoBehaviour
         dirX = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-        if(Input.GetButtonDown("Jump")){
+        if(Input.GetButtonDown("Jump") && isGrounded() ){
             rb.velocity = new Vector2(rb.velocity.x,jumpForce);
         }
 
@@ -61,5 +66,9 @@ public class PlayerMovement : MonoBehaviour
         }
 
         anim.SetInteger("state", (int)state); // state is a enum 
+    }
+
+    private bool isGrounded(){
+        return Physics2D.BoxCast(coll.bounds.center,coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
